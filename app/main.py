c5907965c9780e91a1e2641313a29e2d92007d64
manutonial <manumanusi2014@gmail.com>
2026-04-22 16:19:52 -0300

    fix: route endpoints in main.py

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
-
+from app.api.routes import router
 app = FastAPI(
     title="Service Monitor API",
     description="Api for health check third party services",
     version="1.0.0"
 )
+app.include_router(router, prefix="/api/v1")
 
 @app.get("/", tags=["Health"])
 async def root():
