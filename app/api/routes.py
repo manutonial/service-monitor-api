521d3574aaaa9bd47a74256683c97e02ea4f1b37
manutonial <manumanusi2014@gmail.com>
2026-04-27 18:34:38 -0300

    feat(routes.py): add pagination and status code for errors

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,3 +1,6 @@
+
+from typing import Annotated
+
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Query, Session
@@ -19,7 +22,10 @@ def health():
     return {"status": "ok"}
 
 @router.post("/services", response_model=ServiceResponse, status_code=201)
-def create_service(payload: ServiceCreate, db: Session = Depends(get_db)):
+def create_service(
+    payload: ServiceCreate,
+    db: Annotated[Session, Depends(get_db)]
+):
     try:
         return service_repository.create(db, payload)
     # If the url already exists in the database, returns HTTPException
@@ -28,18 +34,24 @@ def create_service(payload: ServiceCreate, db: Session = Depends(get_db)):
         raise HTTPException(status_code=409, detail="URL already has been created")
 
 @router.get("/services", response_model=list[ServiceResponse])
-def list_services(db: Session = Depends(get_db), skip: int = Query(default=0), limit: int = Query(default=100, le=500)):
+def list_services(
+    db: Annotated[Session, Depends(get_db)], 
+    skip: int = Query(default=0), 
+    limit: int = Query(default=100, le=500)
+):
     return service_repository.list_all(db, skip=skip, limit=limit)
 
 @router.post("/services/{service_id}/check", response_model=CheckResultResponse)
-async def check_service(service_id: int, db: Session = Depends(get_db)):
+async def check_service(service_id: int, db: Annotated[Session, Depends(get_db)]):
     result = await monitor_service.run_check(db, service_id)
-
     if not result:
-        raise HTTPException(status_code=404, detail="Service not found")
-
+        raise HTTPException(status_code=404, detail="Service not found.")
     return result
 
 @router.get("/services/{service_id}/history", response_model=list[CheckResultResponse])
-def get_history(service_id: int, db: Session = Depends(get_db)):
-    return check_result_repository.list_by_service(db, service_id)
+def get_history(
+    service_id: int,
+    db: Annotated[Session, Depends(get_db)],
+    limit: int = Query(default=50, ge=1, le=200)
+):
+    return check_result_repository.list_by_service(db, service_id, limit=limit)
