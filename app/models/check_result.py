04687df7cbb9ce6376281d5807e857846ad79b54
manutonial <manumanusi2014@gmail.com>
2026-04-30 11:53:06 -0300

    feat(check_result): modify status nullable and make relationship with service model

--- app/models/check_result.py
+++ app/models/check_result.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Boolean, Column, DateTime, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 
 from app.core.database import Base
@@ -9,8 +10,10 @@ class CheckResult(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     service_id = Column(Integer, ForeignKey("services.id"), nullable=False, index=True)
-    status = Column(Integer, nullable=False)
+    status = Column(Integer, nullable=True)
     response_time_ms = Column(Integer, nullable=True)
     is_up = Column(Boolean, nullable=False)
     error_message = Column(String, nullable=True)
     checked_at = Column(DateTime(timezone=True), server_default=func.now())
+
+    service = relationship("Service", back_populates="checks")
