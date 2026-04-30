7d5e388cd3ec3697a94befe100215c8a82ba9ef8
manutonial <manumanusi2014@gmail.com>
2026-04-30 11:49:49 -0300

    feat(service): check registers gonna be deleted with the related service

--- app/models/service.py
+++ app/models/service.py
@@ -1,6 +1,7 @@
-from datetime import datetime
 
 from sqlalchemy import Column, DateTime, Integer, String
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
 
 from app.core.database import Base
 
@@ -12,4 +13,10 @@ class Service(Base):
     name = Column(String, nullable=False)
     url = Column(String, nullable=False, unique=True)
     expected_status = Column(Integer, default=200)
-    created_at = Column(DateTime, default=datetime.now)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+    checks = relationship(
+        "CheckResult",
+        back_populates="service",
+        cascade="all, delete-orphan",
+    )
